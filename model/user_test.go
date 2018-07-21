dde35633f0bf7e5184971e097b8d949f5bde5436
Victor Franco <victorfrancovl@gmail.com>
2018-07-21 14:09:08 -0300

    Split long test into two smaller ones

--- model/user_test.go
+++ model/user_test.go
@@ -1,20 +1,23 @@
 package model_test
 
 import (
+	"testing"
+
 	"github.com/franela/goblin"
 	"github.com/vtfr/bossanova/model"
-	"testing"
 )
 
 func TestUser(t *testing.T) {
+	user := &model.User{}
+	user.SetPassword("right")
+
 	g := goblin.Goblin(t)
 	g.Describe("User", func() {
-		g.It("Should set and verify a password successfully", func() {
-			user := &model.User{}
-			user.SetPassword("right")
-
-			g.Assert(user.VerifyPassword("wrong")).IsFalse("wrong password")
+		g.It("Should verify a password successfully", func() {
 			g.Assert(user.VerifyPassword("right")).IsTrue("right password")
+		})
+		g.It("Should not verify a wrong password", func() {
+			g.Assert(user.VerifyPassword("wrong")).IsFalse("wrong password")
 		})
 	})
 }
